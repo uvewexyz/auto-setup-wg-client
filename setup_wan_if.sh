2e28dc424ef238d0058628920b2a2fbf5209e7e7
kingz-adit <muhammadaditiyarakhman@gmail.com>
2025-05-18 23:10:38 +0700

    Upload the script setup_wan_if.sh v1.1
    Descriptions:
    - Delete semicolon ";" in the "Restart the network service" section. That section reproduce weird output when running the script

--- setup_wan_if.sh
+++ setup_wan_if.sh
@@ -9,31 +9,28 @@
 echo "Saving all network informations and configurations before changes...";
 sleep 2;
 echo "Backup network configuration...";
-cp /etc/config/network /etc/config/network$(date +%Y%m%d%).bak;
+cp /etc/config/network /tmp/network$(date +%Y_%m_%d_%H_%M_%S).bak;
 echo "-----------------------------------------------------";
-echo -e "Backup network configuration done
-        Result: 
-        $(ls /etc/config/network*)\n";
+echo "Backup network configuration done. Result:"
+echo "$(ls /tmp/network*)";
 echo "------------------------------------------------------";
-sleep 2;
+sleep 5;
 
 echo "Show and save ip address information";
 echo "-----------------------------------------------------";
-ip addr show | tee /tmp/ip_addr$(date +%Y%m%d%).txt;
-echo -e "Show and save ip address information done
-        Result: 
-        $(ls /tmp/ip_addr*)\n";
+ip addr show | tee /tmp/ip_addr$(date +%Y_%m_%d_%H_%M_%S).txt;
+echo "Show and save ip address information done. Result:" 
+echo "$(ls /tmp/ip_addr*)";
 echo "------------------------------------------------------";
-sleep 2;
+sleep 5;
 
 echo "Show and save routing table";
 echo "-----------------------------------------------------";
-route -n | tee /tmp/route$(date +%Y%m%d%).txt;
-echo -e "Show and save routing table done 
-        Result:
-        $(ls /tmp/route*)\n";
+route -n | tee /tmp/route$(date +%Y_%m_%d_%H_%M_%S).txt;
+echo "Show and save routing table done. Result:"
+echo "$(ls /tmp/route*)";
 echo "------------------------------------------------------";
-sleep 2;
+sleep 5;
 
 
 
@@ -47,25 +44,26 @@ uci -q set network.wan="interface";
 uci -q set network.wan.device="eth0";
 uci -q set network.wan.proto="dhcp";
 uci -q commit network;
+echo "-----------------------------------------------------";
 echo "Successfully set up WAN connection. Result:"
 uci show network.wan;
+sleep 5;
 echo "-----------------------------------------------------";
-sleep 2;
 
 # Restart the network service
 echo "Restarting network service...";
 echo "-----------------------------------------------------";
 NET_SVC="/etc/init.d/network"
 if eval $NET_SVC status >/dev/null 2>&1; then
-    echo "Network service state: Running";
-    echo "Restarting network service";
-    sleep 2;
-    eval $NET_SVC restart &;
-    NET_PID=$!;
-    wait $NET_PID;
-    echo "Network service successfully restarted";
+    echo "Network service state: Running"
+    echo "Restarting network service"
+    sleep 2
+    eval $NET_SVC restart &
+    NET_PID=$!
+    wait $NET_PID
+    echo "Network service successfully restarted"
 else
-    echo "WTF, network service is dead, need to be fixed";
+    echo "WTF, network service is dead, need to be fixed"
     exit 1
 fi
 sleep 5;
@@ -73,18 +71,18 @@ sleep 5;
 echo "-----------------------------------------------------";
 echo "Show route information:";
 route -n
-sleep 2;
+sleep 5;
 
 echo "------------------------------------------------------";
 echo "Show ip address information:";
-ip addr show
-sleep 2;
+ip addr show eth0
+sleep 5;
 
 echo "------------------------------------------------------";
 echo "Check ping connection";
 ping -c 3 8.8.8.8
 sleep 8;
 ping -c 3 google.com
-sleep 8;
+sleep 5;
 
 echo "Set up WAN connection finished";
