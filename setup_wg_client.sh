620e0818efdd46cd18ff8c14f35bbb85ee8218b6
kingz-adit <muhammadaditiyarakhman@gmail.com>
2025-05-18 23:41:03 +0700

    Reupload the script setup_wg_client.sh v1
    Descriptions:
    - Add features to automatically setup WireGuard Client. But, you need custom in the WG_SERV and WG_SRV_PUB variable, to match the pub key in your WireGuard server

--- setup_wg_client.sh
+++ setup_wg_client.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 
-##############################################                                                                                                                     
-### Easy setup Wireguard Client in OpenWRT ###                                                                                                                     
-##############################################                                                                                        
+##############################################
+### Easy setup Wireguard Client in OpenWRT ###
+##############################################
 
 
-# Set Variable #                                                                                                                                                   
-WG_DIR="/etc/wireguard"                                                                                                                                            
-WG_IF="wg0"                                                                                                                                                        
-WG_SERV="54.251.3.207"                                                                                                                                             
-# Pilih salah satu mau pakai IP Publik atau Domain! #                                                                                                              
-# WG_DOM="Your_Domain"                                                                                                                                             
+# Set Variable #
+WG_DIR="/etc/wireguard"
+WG_IF="wg0"
+WG_SERV=""
+# Pilih salah satu mau pakai IP Publik atau Domain! #
+# WG_DOM="Your_Domain"
 WG_PORT="51820"
 WG_ADDR="10.10.20.5/24" # Sesuaikan dengan IP Interface WG di Server!
 WG_CLN_KEY="$(cat $WG_DIR/wg.key)" # Client private key
 WG_CLN_PUB="$(cat $WG_DIR/wg.pub)" # Client public key
-WG_SRV_PUB="qAXDUfz7J2moxpTsp1jnTsP88tamegnjx9O9a4mbDUs=" # Server public key
+WG_SRV_PUB="" # Server public key
 NET_SVC="/etc/init.d/network"
 FRW_SVC="/etc/init.d/firewall"
 WAN_IF="$(uci show network.wan.device | awk -F"'" '{print $2}')"
@@ -24,15 +24,18 @@ WAN_GTW="$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1 | 
 
 
 # Install Packages #
+echo "---------------------------------------------------------------";
 echo "Installing Wireguard packages";
 sleep 2;
+echo "---------------------------------------------------------------";
 opkg update && opkg install wireguard-tools kmod-wireguard luci-proto-wireguard;
-echo "Wireguard packages successfully installed. Result:" 
+echo "Wireguard packages successfully installed. Result:";
 echo "$(opkg list-installed | grep wireguard)";
 sleep 2;
 
 
-# Create WG Dir #                                                                                                                                                  
+# Create WG Dir #
+echo "---------------------------------------------------------------";
 echo "Check Wireguard directory";
 sleep 2;
 if [ ! -d ${WG_DIR} ]; then
@@ -44,17 +47,20 @@ sleep 2;
 
 
 # Generate a key pair of private and public keys #
+echo "---------------------------------------------------------------";
 echo "Generate a key pair of private and public keys";                                                                                                             
 sleep 2;
 wg genkey | tee $WG_DIR/wg.key | wg pubkey > $WG_DIR/wg.pub;
+echo "---------------------------------------------------------------";
 echo "Successfully generated a key pairs, result:";
 sleep 2;
-echo "Your private key: $WG_CLN_KEY"                                                                                                                               
-echo "Your public key: $WG_CLN_PUB"                                                                                                                                
+echo "Your private key: $WG_CLN_KEY";
+echo "Your public key: $WG_CLN_PUB";
 sleep 5;
 
 
 # Setup Firewall #
+echo "---------------------------------------------------------------";
 echo "Let's Setup Firewall";
 sleep 2;
 uci rename firewall.@zone[0]="lan";
@@ -62,23 +68,25 @@ uci rename firewall.@zone[1]="wan";
 uci add_list firewall.wan.network="${WG_IF}";
 uci del_list firewall.wan.network="${WG_IF}";
 uci commit firewall;
-#service firewall restart &;
 sleep 2;
-echo "Firewall successfully configured, result: $(uci show firewall.wan.network)";
+echo "---------------------------------------------------------------";
+echo "Firewall successfully configured. Result:"
+echo "$(uci show firewall.wan.network)";
 sleep 2;
 echo "Restart the firewall service"
 if eval $FRW_SVC status >/dev/null 2>&1; then
-    echo "Firewall service state: Running";
-    echo "Restarting firewall service";
-    sleep 2;
-    eval $FRW_SVC restart &;
-    FRW_PID=$!;
-    wait $FRW_PID;
-    echo "Firewall service successfully restarted";
+    echo "Firewall service state: Running"
+    echo "Restarting firewall service"
+    sleep 2
+    eval $FRW_SVC restart &
+    FRW_PID=$!
+    wait $FRW_PID
+    echo "Firewall service successfully restarted"
 else
-    echo "WTF, firewall service is dead, need to be fixed";
+    echo "WTF, firewall service is dead, need to be fixed"
     exit 1
 fi
+echo "---------------------------------------------------------------";
 sleep 2;
 
 
@@ -92,17 +100,22 @@ uci set network.${WG_IF}.private_key="${WG_CLN_KEY}";
 uci add_list network.${WG_IF}.addresses="${WG_ADDR}";
 uci set network.wg1.metric="10";
 uci set network.wg1.dns="1.1.1.1";
-echo "Wireguard interface successfully configured, result: $(uci show network.\${WG_IF})";
+echo "---------------------------------------------------------------";
+echo "Wireguard interface successfully configured. Result:";
+echo "$(uci show network.${WG_IF})";
 sleep 2;
 
 
 # Configure WAN interface #
 uci set network.wan.metric="20";
-echo "Successfully setting metric in WAN interface, result: $(uci show network.wan.metric;)";
+echo "---------------------------------------------------------------";
+echo "Successfully setting metric in WAN interface. Result:";
+echo "$(uci show network.wan.metric;)";
 sleep 2;
 
 
 # Add VPN peers #
+echo "---------------------------------------------------------------";
 echo "Let's add VPN peers";
 sleep 2;
 uci -q delete network.wgserver;
@@ -114,24 +127,28 @@ uci set network.wgserver.persistent_keepalive="25";
 uci set network.wgserver.route_allowed_ips="1";
 uci add_list network.wgserver.allowed_ips="0.0.0.0/0";
 uci commit network;
-echo "VPN peers successfully added, result: $(uci show network.wgserver)";
+echo "---------------------------------------------------------------";
+echo "VPN peers successfully added. Result:";
+echo "$(uci show network.wgserver)";
 sleep 2;
 
 
 # Restart the network service #
+echo "---------------------------------------------------------------";
 echo "Restart the network service"
 if eval $NET_SVC status >/dev/null 2>&1; then
-    echo "Network service state: Running";
-    echo "Restarting network service";
-    sleep 2;
-    eval $NET_SVC restart &;
-    NET_PID=$!;
-    wait $NET_PID;
-    echo "Network service successfully restarted";
+    echo "Network service state: Running"
+    echo "Restarting network service"
+    sleep 2
+    eval $NET_SVC restart &
+    NET_PID=$!
+    wait $NET_PID
+    echo "Network service successfully restarted"
 else
-    echo "WTF, network service is dead, need to be fixed";
+    echo "WTF, network service is dead, need to be fixed"
     exit 1
 fi
+echo "---------------------------------------------------------------";
 sleep 5;
 
 
@@ -145,12 +162,14 @@ ip route del default;
 ip route add default dev $WG_IF;
 ip route add 54.251.3.207/32 via $WAN_GTW dev $WAN_IF;
 echo "Successfully configured routing IP, result: $(route -n)";
+echo "---------------------------------------------------------------";
 sleep 5;
 
 
 # Restart WG Interface #
 echo "Check Wireguard connection";
 wg;
+echo "---------------------------------------------------------------";
 echo "Let's restart Wireguard interface";
 sleep 2;
 ifdown $WG_IF;
@@ -158,6 +177,8 @@ ifup $WG_IF;
 
 
 # Check Connection #
+echo "---------------------------------------------------------------";
 curl ipinfo.io | tee ~/ipinfo$(date +%Y%m%d).txt;
+echo "---------------------------------------------------------------";
 sleep 5;
 traceroute openwrt.org | tee /root/traceroute.txt;
