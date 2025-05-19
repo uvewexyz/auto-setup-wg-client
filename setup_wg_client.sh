a027583060521c791659eca78717e90f5cc85283
kingz-adit <muhammadaditiyarakhman@gmail.com>
2025-05-19 11:23:44 +0700

    Update the script setup_wg_client.sh v1.1
    Descriptions:
    - Adding the descriptions every variable
    - Fixing the weird output at running this script

--- setup_wg_client.sh
+++ setup_wg_client.sh
@@ -1,25 +1,47 @@
 #!/bin/sh
 
-
 ##############################################
 ### Easy setup Wireguard Client in OpenWRT ###
 ##############################################
 
-
 # Set Variable #
+# The WireGuard dir variable
 WG_DIR="/etc/wireguard"
+# The WireGuard interface variable
 WG_IF="wg0"
+##############################################
+# You must choose between using the public IP 
+# with the WG_SERV variable or using the domain 
+# from your server WireGuard with the WG_DOM variable.
+# Select one and comment on the not selected variable. 
+# The default is using the WG_SERV variable. 
+# If you use the WG_DOM, please uncomment in the "Add VPN peers" section too. 
+# Example:
+#
+# 1. Using the public IP as endpoint server.
+# WG_SERV="12.34.56.78"
+# #WG_DOM="www.example.com"
+# 2. Using the domain as endpoint:
+# #WG_SERV="12.34.56.78"
+# WG_DOM="www.example.com"
+##############################################
+# The public IP of your server WireGuard
 WG_SERV=""
-# Pilih salah satu mau pakai IP Publik atau Domain! #
-# WG_DOM="Your_Domain"
+# The domain of your server WireGuard
+#WG_DOM="Your_Domain"
+# The port of your server WireGuard
 WG_PORT="51820"
-WG_ADDR="10.10.20.5/24" # Sesuaikan dengan IP Interface WG di Server!
-WG_CLN_KEY="$(cat $WG_DIR/wg.key)" # Client private key
-WG_CLN_PUB="$(cat $WG_DIR/wg.pub)" # Client public key
-WG_SRV_PUB="" # Server public key
+# Matching with the WG ip interface in your server configuration!
+WG_ADDR="10.10.20.5/24"
+# Matching with the Server public key!
+WG_SRV_PUB=""
+# Network service variable
 NET_SVC="/etc/init.d/network"
+# Firewall service variable
 FRW_SVC="/etc/init.d/firewall"
+# The WAN interface variable
 WAN_IF="$(uci show network.wan.device | awk -F"'" '{print $2}')"
+# The WAN gateway variable
 WAN_GTW="$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1 | sed 's/\.[0-9]\{1,3\}$/\.1/')"
 
 
@@ -28,7 +50,7 @@ echo "---------------------------------------------------------------";
 echo "Installing Wireguard packages";
 sleep 2;
 echo "---------------------------------------------------------------";
-opkg update && opkg install wireguard-tools kmod-wireguard luci-proto-wireguard;
+opkg update && opkg install wireguard-tools kmod-wireguard luci-proto-wireguard curl;
 echo "Wireguard packages successfully installed. Result:";
 echo "$(opkg list-installed | grep wireguard)";
 sleep 2;
@@ -54,6 +76,10 @@ wg genkey | tee $WG_DIR/wg.key | wg pubkey > $WG_DIR/wg.pub;
 echo "---------------------------------------------------------------";
 echo "Successfully generated a key pairs, result:";
 sleep 2;
+# Client private key
+WG_CLN_KEY="$(cat $WG_DIR/wg.key)"
+# Client public key
+WG_CLN_PUB="$(cat $WG_DIR/wg.pub)"
 echo "Your private key: $WG_CLN_KEY";
 echo "Your public key: $WG_CLN_PUB";
 sleep 5;
@@ -121,6 +147,7 @@ sleep 2;
 uci -q delete network.wgserver;
 uci set network.wgserver="wireguard_${WG_IF}";
 uci set network.wgserver.public_key="${WG_SRV_PUB}";
+#uci set network.wgserver.endpoint_host="${WG_DOM}";
 uci set network.wgserver.endpoint_host="${WG_SERV}";
 uci set network.wgserver.endpoint_port="${WG_PORT}";
 uci set network.wgserver.persistent_keepalive="25";
@@ -156,12 +183,14 @@ sleep 5;
 echo "Let's configure routing IP";
 sleep 2;
 echo "Check routing table before";
-route -n | tee ~/routing_before$(date +%Y%m%d).txt;
+route -n | tee /tmp/routing_before$(date +%Y_%m_%d_%H_%M_%S).txt;
 sleep 2;
 ip route del default;
 ip route add default dev $WG_IF;
 ip route add 54.251.3.207/32 via $WAN_GTW dev $WAN_IF;
-echo "Successfully configured routing IP, result: $(route -n)";
+echo "Successfully configured routing IP";
+echo "Result:";
+echo "$(route -n)";
 echo "---------------------------------------------------------------";
 sleep 5;
 
@@ -169,16 +198,20 @@ sleep 5;
 # Restart WG Interface #
 echo "Check Wireguard connection";
 wg;
+sleep 2;
 echo "---------------------------------------------------------------";
 echo "Let's restart Wireguard interface";
-sleep 2;
 ifdown $WG_IF;
+sleep 5;
 ifup $WG_IF;
+echo "---------------------------------------------------------------";
+echo "Successfully restarted Wireguard interface";
+ifconfig wg0;
 
 
 # Check Connection #
 echo "---------------------------------------------------------------";
-curl ipinfo.io | tee ~/ipinfo$(date +%Y%m%d).txt;
+curl ipinfo.io | tee /tmp/ipinfo$(date +%Y_%m_%d_%H_%M_%S).txt;
 echo "---------------------------------------------------------------";
 sleep 5;
-traceroute openwrt.org | tee /root/traceroute.txt;
+traceroute openwrt.org | tee /tmp/traceroute$(date +%Y_%m_%d_%H_%M_%S).txt;
